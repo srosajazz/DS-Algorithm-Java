74c5e738e50e26e565d36a799835107ccf07093c
Sergio Rosa <scarlosjazz@gmail.com>
2019-01-31 09:18:04 -0500

    Vector & Arrays

---
+++ src/com/sergiorosa/ds/vector/Vector.java
@@ -0,0 +1,10 @@
+package com.sergiorosa.ds.vector;
+
+public class Vector {
+
+	private String[] elementos;
+	
+	public Vector(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
+}
---
+++ src/com/sergiorosa/ds/vector/test/app02.java
@@ -0,0 +1,15 @@
+package com.sergiorosa.ds.vector.test;
+
+import com.sergiorosa.ds.vector.Vector;
+
+public class app02 {
+
+	public static void main(String[] args) {
+		
+		Vector vector = new Vector(5);
+		
+		int i=0;
+		i++;
+	}
+
+}
