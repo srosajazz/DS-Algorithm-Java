c8ed3b494f8055fc203b5bef25cd60a5820e8ab5
Sergio Rosa <scarlosjazz@gmail.com>
2019-01-31 14:28:59 -0500

    Adding element in the final of the Array

--- src/com/sergiorosa/ds/vector/Vector.java
+++ src/com/sergiorosa/ds/vector/Vector.java
@@ -2,9 +2,43 @@ package com.sergiorosa.ds.vector;
 
 public class Vector {
 
-	private String[] elementos;
-	
+	private String[] elementos; // option#1
+	private int tamanho;
+
 	public Vector(int capacidade) {
 		this.elementos = new String[capacidade];
+		this.tamanho = 0;
+
+	}
+
+	public boolean adiciona(String elemento) {
+		
+		if (this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;	
+			return true;
+		}
+		return false; 
 	}
 }
+
+
+
+//Options to add elements in the array.
+// Add element in end of the Vector (Option#1)
+/*
+ * public void adiciona(String elemento) { for(int i=0; i<this.elementos.length;
+ * i++) { if(this.elementos[i] == null) { this.elementos[i]= elemento; break; }
+ * 
+ * } }
+ */
+
+/*public void adiciona(String elemento) throws Exception {
+
+	if (this.tamanho < this.elementos.length) {
+		this.elementos[this.tamanho] = elemento;
+		this.tamanho++;
+	} else {
+		throw new Exception("Vector is full, is not possible to add more elementos in the Vector.");
+	}
+}*/
---
+++ src/com/sergiorosa/ds/vector/test/app03.java
@@ -0,0 +1,14 @@
+package com.sergiorosa.ds.vector.test;
+
+import com.sergiorosa.ds.vector.Vector;
+
+public class app03 {
+	public static void main(String[] args) {
+
+		Vector vector = new Vector(2);
+
+		vector.adiciona("elemento 1");
+		vector.adiciona("elemento 2");
+		vector.adiciona("elemento 3");
+	}
+}
